13ee7dc227da16d9259e7012d1800595015702e0
xFEF0 <52141168+xFEF0@users.noreply.github.com>
2021-05-16 14:21:51 -0300

    add exception message

--- restful/src/main/java/com/xfef0/restful/exception/EmployeeNotFoundException.java
+++ restful/src/main/java/com/xfef0/restful/exception/EmployeeNotFoundException.java
@@ -2,5 +2,6 @@ package com.xfef0.restful.exception;
 
 public class EmployeeNotFoundException extends RuntimeException {
     public EmployeeNotFoundException(Long id) {
+        super("Could not find employee " + id);
     }
 }
