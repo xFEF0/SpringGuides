df276e2205db2201fcca3d09c99e54590539d65d
xFEF0 <52141168+xFEF0@users.noreply.github.com>
2021-05-22 13:58:44 -0300

    initial commit for SpringBoot (with security) + Angular app

---
+++ security/src/main/java/com/xfef0/security/SecurityApplication.java
@@ -0,0 +1,30 @@
+package com.xfef0.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+@SpringBootApplication
+@RestController
+public class SecurityApplication {
+
+  @RequestMapping("/resource")
+  @ResponseBody
+  public Map<String, Object> home() {
+    Map<String, Object> model = new HashMap<>();
+    model.put("id", UUID.randomUUID().toString());
+    model.put("content", "Hello, World!");
+    return model;
+  }
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityApplication.class, args);
+	}
+
+}
