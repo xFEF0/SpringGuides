1f6f5e642bdb02bff11e2c7038e511804d394a67
xFEF0 <52141168+xFEF0@users.noreply.github.com>
2021-05-23 19:17:20 -0300

    add login component

--- security/src/main/java/com/xfef0/security/SecurityApplication.java
+++ security/src/main/java/com/xfef0/security/SecurityApplication.java
@@ -2,10 +2,14 @@ package com.xfef0.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
@@ -14,6 +18,11 @@ import java.util.UUID;
 @RestController
 public class SecurityApplication {
 
+  @RequestMapping("/user")
+  public Principal user(Principal user) {
+    return user;
+  }
+
   @RequestMapping("/resource")
   @ResponseBody
   public Map<String, Object> home() {
@@ -21,6 +30,18 @@ public class SecurityApplication {
     model.put("id", UUID.randomUUID().toString());
     model.put("content", "Hello, World!");
     return model;
+  }
+
+  @Configuration
+  protected static class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+      http.httpBasic()
+        .and()
+        .authorizeRequests()
+        .antMatchers("/index.html", "/", "/home", "/login").permitAll()
+        .anyRequest().authenticated();
+    }
   }
 
 	public static void main(String[] args) {
