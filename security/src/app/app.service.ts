0b348ee4a2451676b83cf1d7e24b8296b39f3a4a
xFEF0 <52141168+xFEF0@users.noreply.github.com>
2021-05-24 21:37:09 -0300

    this example does not work correctly

--- security/src/app/app.service.ts
+++ security/src/app/app.service.ts
@@ -12,9 +12,8 @@ export class AppService {
     const headers = new HttpHeaders(credentials ? {
       authorization : 'Basic ' + btoa(credentials.username + ':' + credentials.password)
     } : {});
-    this.http.get('user', {headers: headers})
-      .subscribe(response => {
-        if (response['name']) {
+    this.http.get('user', {headers: headers}).subscribe(response => {
+        if (response) {
           this.authenticated = true;
         } else {
           this.authenticated = false;
