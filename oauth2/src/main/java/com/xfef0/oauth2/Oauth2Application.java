1eb1ecf26bca1c9bd2a2c297782f2108369c6601
xFEF0 <52141168+xFEF0@users.noreply.github.com>
2021-05-28 17:29:26 -0300

    configure web security

--- oauth2/src/main/java/com/xfef0/oauth2/Oauth2Application.java
+++ oauth2/src/main/java/com/xfef0/oauth2/Oauth2Application.java
@@ -2,8 +2,13 @@ package com.xfef0.oauth2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.security.web.authentication.ExceptionMappingAuthenticationFailureHandler;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +17,7 @@ import java.util.Map;
 
 @SpringBootApplication
 @RestController
-public class Oauth2Application {
+public class Oauth2Application extends WebSecurityConfigurerAdapter {
 
 	@GetMapping("/user")
 	public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
@@ -21,6 +26,18 @@ public class Oauth2Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Oauth2Application.class, args);
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests(
+				a -> a.antMatchers("/", "/error", "/webjars/**").permitAll()
+					.anyRequest().authenticated()
+		)
+		.exceptionHandling(
+				e -> e.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+		)
+		.oauth2Login();
 	}
 
 }
