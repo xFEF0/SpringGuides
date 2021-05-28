c972b32260f347ac2c79228f7778a457251410a9
xFEF0 <52141168+xFEF0@users.noreply.github.com>
2021-05-28 17:50:06 -0300

    add csrf protection for logout

--- oauth2/src/main/java/com/xfef0/oauth2/Oauth2Application.java
+++ oauth2/src/main/java/com/xfef0/oauth2/Oauth2Application.java
@@ -9,6 +9,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.ExceptionMappingAuthenticationFailureHandler;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -36,6 +37,9 @@ public class Oauth2Application extends WebSecurityConfigurerAdapter {
 		)
 		.exceptionHandling(
 				e -> e.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+		)
+		.csrf(
+				c -> c.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
 		)
 		.logout(
 				l -> l.logoutSuccessUrl("/").permitAll()
