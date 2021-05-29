c7a115e8320ef62c35cac112d0bd111a15990b4a
xFEF0 <52141168+xFEF0@users.noreply.github.com>
2021-05-28 22:31:36 -0300

    generating 401 in the server

--- oauth2/src/main/java/com/xfef0/oauth2/Oauth2Application.java
+++ oauth2/src/main/java/com/xfef0/oauth2/Oauth2Application.java
@@ -2,24 +2,75 @@ package com.xfef0.oauth2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.OAuth2Error;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Collections;
+import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+
+import static org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction.oauth2AuthorizedClient;
 
 @SpringBootApplication
 @RestController
 public class Oauth2Application extends WebSecurityConfigurerAdapter {
+
+	@Bean
+	public WebClient rest(ClientRegistrationRepository clients,
+						  OAuth2AuthorizedClientRepository authz) {
+		ServletOAuth2AuthorizedClientExchangeFilterFunction oauth2 = new
+				ServletOAuth2AuthorizedClientExchangeFilterFunction(clients, authz);
+		return WebClient.builder().filter(oauth2).build();
+	}
+
+	@Bean
+	public OAuth2UserService<OAuth2UserRequest, OAuth2User> oAuth2UserService(WebClient rest) {
+		DefaultOAuth2UserService delegate = new DefaultOAuth2UserService();
+		return request -> {
+			OAuth2User user = delegate.loadUser(request);
+			if (!"github".equals(request.getClientRegistration().getRegistrationId())) {
+				return user;
+			}
+
+			OAuth2AuthorizedClient client = new OAuth2AuthorizedClient(request.getClientRegistration(),
+					user.getName(), request.getAccessToken());
+			String url = user.getAttribute("organizations_url");
+			List<Map<String, Object>> orgs = rest
+					.get().uri(url)
+					.attributes(oauth2AuthorizedClient(client))
+					.retrieve()
+					.bodyToMono(List.class)
+					.block();
+
+			if (Objects.requireNonNull(orgs).stream().anyMatch(
+					org -> "spring-projects".equals(org.get("login")))) {
+						return user;
+			}
+			throw new OAuth2AuthenticationException(new OAuth2Error("invalid_token",
+					"Not in Spring Team", ""));
+		};
+	}
 
 	@GetMapping("/user")
 	public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
@@ -37,8 +88,7 @@ public class Oauth2Application extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		SimpleUrlAuthenticationFailureHandler handler = new SimpleUrlAuthenticationFailureHandler("/");
 
-		http.antMatcher("/**")
-			.authorizeRequests(
+		http.authorizeRequests(
 				a -> a.antMatchers("/", "/error", "/webjars/**").permitAll()
 					.anyRequest().authenticated()
 			)
