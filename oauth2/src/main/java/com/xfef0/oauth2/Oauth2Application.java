dc9eec3a9d7bf872d3675d08f2783c67fca3d2fd
xFEF0 <52141168+xFEF0@users.noreply.github.com>
2021-05-28 21:17:21 -0300

    add error

--- oauth2/src/main/java/com/xfef0/oauth2/Oauth2Application.java
+++ oauth2/src/main/java/com/xfef0/oauth2/Oauth2Application.java
@@ -7,12 +7,13 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.security.web.authentication.ExceptionMappingAuthenticationFailureHandler;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Collections;
 import java.util.Map;
 
@@ -25,26 +26,43 @@ public class Oauth2Application extends WebSecurityConfigurerAdapter {
 		return Collections.singletonMap("name", principal.getAttribute("name"));
 	}
 
-	public static void main(String[] args) {
-		SpringApplication.run(Oauth2Application.class, args);
+	@GetMapping("/error")
+	public String error(HttpServletRequest request) {
+		String message = (String) request.getSession().getAttribute("error.message");
+		request.getSession().removeAttribute("error.message");
+		return message;
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests(
+		SimpleUrlAuthenticationFailureHandler handler = new SimpleUrlAuthenticationFailureHandler("/");
+
+		http.antMatcher("/**")
+			.authorizeRequests(
 				a -> a.antMatchers("/", "/error", "/webjars/**").permitAll()
 					.anyRequest().authenticated()
-		)
-		.exceptionHandling(
-				e -> e.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
-		)
-		.csrf(
-				c -> c.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
-		)
-		.logout(
-				l -> l.logoutSuccessUrl("/").permitAll()
-		)
-		.oauth2Login();
+			)
+			.exceptionHandling(
+					e -> e.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+			)
+			.csrf(
+					c -> c.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+			)
+			.logout(
+					l -> l.logoutSuccessUrl("/").permitAll()
+			)
+			.oauth2Login(
+					o -> o.failureHandler(
+							(request, response, exception) -> {
+								request.getSession().setAttribute("error.message", exception.getMessage());
+								handler.onAuthenticationFailure(request, response, exception);
+							}
+					)
+			);
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2Application.class, args);
 	}
 
 }
