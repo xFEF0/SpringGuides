f109d0df9ff2c551102f186897888a369dfa970b
xFEF0 <52141168+xFEF0@users.noreply.github.com>
2021-05-29 13:56:11 -0300

    initial commit for service

---
+++ metrics/service/src/main/java/com/xfef0/service/ServiceApplication.java
@@ -0,0 +1,16 @@
+package com.xfef0.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class ServiceApplication {
+
+	public static void main(String[] args) {
+		log.info("starting server");
+		SpringApplication.run(ServiceApplication.class, args);
+	}
+
+}
